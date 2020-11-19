81ea4e69aa8553e0317c94d2dd1dac2bdaaba9df
AHT <alehertoro725@gmail.com>
2020-11-18 20:26:55 -0500

    Updated the projects Methods and added Methods part 2

--- _2nd Range/2_Methods/Program.cs
+++ _2nd Range/2_Methods/Program.cs
@@ -23,6 +23,11 @@ namespace _2_Methods
             Minus(6);
             Minus(6, 3);
 
+            next();
+
+            inta();
+            inta2();
+
             Console.ReadLine();
         }
 
@@ -47,5 +52,52 @@ namespace _2_Methods
             Console.WriteLine("The result is: " + result);
             return result;
         }
+
+        //Passing arguments
+
+        //As value
+        static void Sqr(int x)
+        {
+            x = x * x;
+        }
+
+        static void inta()
+        {
+            int a = 3;
+            Sqr(a); // Will execute the command but will not change it in the method
+
+            Console.WriteLine(a);
+        }
+
+        //As reference
+        static void Sqr2(ref int x) //to be able to change the value, you'd need to use the keyword "ref"
+        {
+            x = x * x;
+        }
+
+        static void inta2()
+        {
+            int a = 3;
+            Sqr2(ref a); //it has to be called as a "ref"
+
+            Console.WriteLine(a);
+        }
+
+        //As exit
+        static void getValues(out int x, out int y) //When you set the parameters as exit parameters, you can use the declared variables inside it to call it in another method
+        {
+            x = 5;
+            y = 42;
+        }
+
+        static void inta3()
+        {
+            int a, b;
+            getValues(out a, out b); //The method will be called and the asigned values will be the ones defined inside the method
+
+            Console.WriteLine(a + "," + b);
+        }
+
+        //This project is very bloated and filled already, Continuation in part 2
     }
 }
---
+++ _2nd Range/3_Methods_Part_2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Methods_Part_2
+{
+    class Program
+    {
+        static void Main(string[] args) //Main Method
+        {
+            Printer(2);
+            Printer(3, 4);
+
+            next();
+
+            Console.WriteLine(Fact(5));
+
+            next();
+
+            DrawPyramid(5);
+
+            Console.ReadLine();
+        }
+
+        static void next()
+        {
+            Console.WriteLine("----------NEXT----------");
+        }
+
+        //Overloading Methods
+
+        static void Printer(int a)
+        {
+            Console.WriteLine(a);
+        }
+
+        static void Printer(int b, int c)
+        {
+            Console.WriteLine(b + ", " + c);
+        }
+
+        //^^^^Both methods have the same name, however, they have different parameters, which differentiates them
+
+        //Recursive Methods
+
+        static int Fact(int num)
+        {
+            if(num == 1)
+            {
+                return 1;
+            }
+
+            return num * Fact(num - 1); //A recursive method is a method that calls itself inside the method
+        }
+
+        //Pyramid: Here's a funny game, drawing a pyramid
+
+        static void DrawPyramid(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = i; j <= n; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int k = 1; k <= 2 + i - 1; k++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
