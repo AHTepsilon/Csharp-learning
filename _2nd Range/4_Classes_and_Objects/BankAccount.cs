e7a8f6e96cd4a91add7f1a61fcdc33c6c0077642
AHT <alehertoro725@gmail.com>
2020-11-19 22:27:26 -0500

    Added classes and objects lesson

---
+++ _2nd Range/4_Classes_and_Objects/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Classes_and_Objects
+{
+    class BankAccount
+    {
+        private String name;
+
+        public String Name //getters and setters for private values are set in a special type of constructor
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int id;
+
+        private int amountSaved;
+
+        public int AmountSaved
+        {
+            get { return amountSaved; }
+            set { amountSaved = value; }
+        }
+
+        public BankAccount(String name, int id, int amountSaved)
+        {
+            this.name = name;
+            this.id = id;
+            this.amountSaved = amountSaved;
+        }
+
+        public void printer()
+        {
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("id: " + id);
+            Console.WriteLine("amount saved in the bank: " + amountSaved);
+        }
+    }
+}
---
+++ _2nd Range/4_Classes_and_Objects/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Classes_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount1 = new BankAccount("Wallace", 8433256, 2551);
+            BankAccount bankAccount2 = new BankAccount("Roderick", 1421446, 823);
+            BankAccount bankAccount3 = new BankAccount("Linda", 05431405, 2924);
+
+            bankAccount1.printer();
+            bankAccount2.printer();
+            bankAccount3.printer();
+
+            bankAccount1.id = 64332834; //A public variable in the class can be changed as you please
+            bankAccount1.Name = "Johnathan"; //When the set and get are added, the private variable can be modified with the normal method for modifying
+            bankAccount1.AmountSaved = 1000;
+
+            bankAccount1.printer();
+
+            Console.ReadLine();
+
+            //get and set
+        }
+    }
+}
